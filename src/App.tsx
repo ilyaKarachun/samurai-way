02e9fa0f678b1e5dbffb3ccf3c154d712e8a2807
Ilya_Karachun <Karachun_ilya94@mail.ru>
2022-07-20 13:44:21 +0300

    refactor: edit link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
+import {Technologies} from "./Technologies";
+import {Header} from "./Header";
+import {BrowserRouter, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <Technologies/>
+                <Route path={"/hello"} render={() => <HelloMessage message={"Hello friends"}/>}/>
+                <Route path={"/bye"} render={() => <ByeMessage message={"Bye samurai"}/>}/>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+type HelloMessageType = {
+    message: string
+}
+
+const HelloMessage: FC<HelloMessageType> = (props) => {
+    return <h1>{props.message}</h1>
+}
+
+const ByeMessage: FC<HelloMessageType> = (props) => {
+    return <h1>{props.message}</h1>
 }
 
 export default App;
