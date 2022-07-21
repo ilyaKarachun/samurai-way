f5561f0776d003d92ae9286b4532ed2278faec58
Ilya_Karachun <Karachun_ilya94@mail.ru>
2022-07-21 13:07:35 +0300

    feat: component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://skidka02.ru/800/600/https/i.etsystatic.com/10919371/r/il/b56d68/1896464555/il_fullxfull.1896464555_qh40.jpg"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img
+                src="https://img.desktopwallpapers.ru/nature/pics/wide/1920x1200/e862cdd94c26e6601ea2380ef07e44f7.jpg"/>
+            </div>
+            <div><img
+                src="https://avatars.mds.yandex.net/i?id=36fc156aac73eed5b13a0e4f881212ac-6191070-images-thumbs&n=13"/>
+            </div>
+            <div>avatar+description</div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
